deebd2b02f446170996e9b5bcfed2aab07b3e25f
Pawel <email@gmail.com>
2023-05-15 19:14:53 +0200

    Writing first unittest, testing url resolves to login page view

--- User_App/tests.py
+++ User_App/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.urls import resolve
+from django.test import TestCase
+from User_App.views import login_page
+
+
+class LoginPageTest(TestCase):
+
+    def test_url_resolves_to_login_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, login_page)
