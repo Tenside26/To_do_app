5ff18dc89da98bf2a0df7d7b0e69f30436707a45
Pawel <email@gmail.com>
2023-05-15 18:33:20 +0200

    Adding functional_test.py and blueprint for my functional tests

---
+++ functional_tests.py
@@ -0,0 +1,17 @@
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+# User has heard about a cool new online to-do app. He goes to check out its homepage
+browser.get('http://127.0.0.1:8000/')
+
+# He notices the login page with register option
+
+# He clicks on "i want to register" button and gets redirected to register page
+
+# He fill out the register form and clicks register button
+
+# He gets redirected to login page with message saying that he registered that account
+
+
