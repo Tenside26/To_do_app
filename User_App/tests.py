9a29d8147be8bc2483331bd4a6f26e870ca6001e
Pawel <email@gmail.com>
2023-05-17 18:16:18 +0200

    Adding test for UserLoginForm

--- User_App/tests.py
+++ User_App/tests.py
@@ -2,6 +2,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from User_App.views import login_page
+from .forms import UserLoginForm
 
 
 class LoginPageTest(TestCase):
@@ -13,3 +14,9 @@ class LoginPageTest(TestCase):
     def test_login_page_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'login.html')
+
+    def test_login_page_form_rendering(self):
+        form_data = {'username': 'something1',
+                     'password': 'something2'}
+        form = UserLoginForm(data=form_data)
+        self.assertTrue(form.is_valid())
