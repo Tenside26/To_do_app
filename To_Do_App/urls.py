48503fe23129fbe4e7525eaedb759b078f0b262d
Pawel <email@gmail.com>
2023-05-24 19:25:35 +0200

    Creating urls.py in Main_App and linking it with To_Do_App urls.py

--- To_Do_App/urls.py
+++ To_Do_App/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("User_App.urls"))
+    path('', include("User_App.urls")),
+    path('main', include("Main_App.urls"))
 ]
