66404b7ab50ba92551ce08bf76f504e2806984b9
Pawel <email@gmail.com>
2023-05-24 19:48:03 +0200

    Creating models.py blueprint

--- Main_App/models.py
+++ Main_App/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskList(models.Model):
+    # Models Still in Progress
+    task_list_owner = #ForeignKey To User
+    task_list_item =#ManyToMany To Task
+
+class Task(models.Model):
+    # Models Still in Progress
+    task_user = # Potential User Who Added That Task
+    task_title = models.CharField(max_length=80)
+    task_description = models.TextField(max_length=1000)
+    task_creation_date = models.DateTimeField(auto_now_add=True)
+
+
+
