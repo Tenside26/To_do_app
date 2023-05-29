9b4e4316b48167d2a82448612665cce4a3b1957d
Pawel <email@gmail.com>
2023-05-29 18:04:47 +0200

    Adding Task model and TaskList model

--- Main_App/models.py
+++ Main_App/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+
+    title = models.CharField(max_length=80)
+    description = models.TextField(max_length=1000)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+
+class UserTaskList(models.Model):
+
+    owner =  models.OneToOneField(User, on_delete=models.CASCADE, related_name="task_list_owner")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="task")
+
+
 
