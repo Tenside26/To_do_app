e04e237781632f95d53f6a8fb2f380903562303e
Pawel <email@gmail.com>
2023-05-29 18:29:29 +0200

    Creating signals.py and adding post_save signal

--- Main_App/apps.py
+++ Main_App/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Main_App'
+
+    def ready(self):
+        import Main_App.signals
