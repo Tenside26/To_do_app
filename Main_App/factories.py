352ee887dcd536a3d5e81769877915e75c7fd840
Pawel <email@gmail.com>
2023-06-05 19:15:05 +0200

    refactoring factories.py using faker

--- Main_App/factories.py
+++ Main_App/factories.py
@@ -1,18 +1,21 @@
 import factory
 from django.contrib.auth.models import User
 from .models import Task, TaskList
-from django.utils import timezone
+from faker import Faker
+
+
+fake = Faker()
 
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    username = "placeholder"
-    password = "placeholder"
-    first_name = "placeholder"
-    last_name = "placeholder"
-    email = "placeholder@gmail.com"
+    username = fake.username()
+    password = fake.password()
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    email = fake.email()
 
 
 class TaskListFactory(factory.django.DjangoModelFactory):
@@ -26,8 +29,8 @@ class TaskFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Task
 
-    title = "placeholder"
-    description = "placeholder"
-    creation_date = timezone.now
-    completed = False
+    title = fake.sentence(nb_words=2)
+    description = fake.sentence(nb_words=20)
+    creation_date = fake.date()
+    completed = fake.boolean()
     task_list = factory.SubFactory(TaskListFactory)
--- To_Do_App/settings.py
+++ To_Do_App/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_faker',
     'User_App',
     'Main_App',
 ]
