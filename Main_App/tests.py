92696f73764b4e2c1940d640abc41a2dc8de5cca
Pawel <email@gmail.com>
2023-06-05 18:34:06 +0200

    Adding Faker and writing some more tests

--- Main_App/tests.py
+++ Main_App/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from .factories import TaskFactory, TaskListFactory, UserFactory
 from pytest_factoryboy import register
+import factory
+from django.db.models import signals
+
 
 register(TaskFactory)
 register(TaskListFactory)
@@ -8,11 +11,18 @@ register(UserFactory)
 
 
 class TaskListTest(TestCase):
-    pass
+
+    def test_task_list_model(self):
+        with factory.django.mute_signals(signals.post_save):
+            task_list = TaskListFactory()
+            assert task_list.owner == UserFactory()
 
 
 class TaskTest(TestCase):
 
-    def test_str_return(self, task_factory):
-        task = task_factory(title="new-title")
-        assert task.__str__() == "new-title"
+    def test_task_model(self):
+        with factory.django.mute_signals(signals.post_save):
+            task = TaskFactory()
+            assert task.title == "placeholder"
+            assert task.description == "placeholder"
+            assert task.completed is False
--- To_Do_App/settings.py
+++ To_Do_App/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_faker',
     'User_App',
     'Main_App',
 ]
